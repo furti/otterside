4e3a41a7ffae04deecda99a636d4353d10456d1d
furti <daniel.furtlehner@gmx.net>
2016-01-10 18:46:31 +0100

    Added gulp-connect and tasks for assets and html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 var gulp = require('gulp'),
   ts = require('gulp-typescript'),
-  tsconfig = require('./tsconfig.json');
+  connect = require('gulp-connect'),
+  tsconfig = require('./tsconfig.json'),
+  assetSource = './src/web/assets/**',
+  htmlSource = './src/web/**/*.html';
 
 var tsProject = ts.createProject('./tsconfig.json');
 
-gulp.task('build', function() {
+gulp.task('assets', function() {
+  return gulp.src(assetSource)
+    .pipe(gulp.dest('./target/assets'));
+});
+
+gulp.task('html', function() {
+  return gulp.src(htmlSource)
+    .pipe(gulp.dest('./target'));
+});
+
+gulp.task('ts', function() {
   var result = tsProject.src()
     .pipe(ts(tsProject));
 
   return result.js.pipe(gulp.dest(tsconfig.compilerOptions.outDir));
 });
 
-gulp.task('watch', ['build'], function() {
-  gulp.watch(tsconfig.filesGlob, ['build']);
+gulp.task('watch', ['ts', 'assets', 'html'], function() {
+  connect.server({
+    root: 'target'
+  });
+
+  gulp.watch(tsconfig.filesGlob, ['scripts']);
+  gulp.watch(assetSource, ['assets']);
+  gulp.watch(htmlSource, ['html']);
 });
