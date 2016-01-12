8b1e4fab33bd8f5a97c483a5b8b61e6e370a3c9c
furti <daniel.furtlehner@gmx.net>
2016-01-12 22:21:18 +0100

    Fixed double comma

--- src/web/scripts/MapUtils.ts
+++ src/web/scripts/MapUtils.ts
@@ -2,7 +2,7 @@
 
 module Otterside {
     export module MapUtils {
-        export function findObjectsByType<ObjectType>(map: Phaser.Tilemap, layer: string,, type: string): Array<ObjectType> {
+        export function findObjectsByType<ObjectType>(map: Phaser.Tilemap, layer: string, type: string): Array<ObjectType> {
             var array: ObjectType[] = [];
 
             for (var object of map.objects[layer]) {
