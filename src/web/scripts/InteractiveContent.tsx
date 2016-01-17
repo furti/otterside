5a44c33840f9449ccde9e779c64faf6d590e8449
furti <daniel.furtlehner@gmx.net>
2016-01-17 10:46:04 +0100

    interactive-content can be maximized an minimized now

--- src/web/scripts/InteractiveContent.tsx
+++ src/web/scripts/InteractiveContent.tsx
@@ -3,25 +3,47 @@ module Otterside {
     export class InteractiveContent extends React.Component<{}, InteractiveContentState> {
         public static contentComponent: InteractiveContent;
 
+        constructor() {
+            super();
+            this.state = {
+                maximized: false
+            };
+        }
+
+        public minimize() {
+            this.setState({
+                maximized: false
+            });
+        }
+
+        public maximize() {
+            this.setState({
+                maximized: true
+            });
+        }
+
         render() {
-            return <div>
+            //Use refs to get active component
+            return <div className={this.state.maximized ? 'maximized interactive-content' : 'interactive-content'}>
                 <h2 className="interactive-header">
                     Otterside
                     <div className="flex">
-                        <button title="Minimize">-</button>
-                        <button title="Maximize">▢</button>
+                        <button title="Minimize" onClick={e => this.minimize() }>-</button>
+                        <button title="Maximize" onClick={e => this.maximize() }>▢</button>
                     </div>
                 </h2>
-                <div className="interactive-container"></div>
+                <div className="interactive-container">
+
+                </div>
             </div>
         }
 
         public static setupContent(): void {
-            InteractiveContent.contentComponent = ReactDOM.render(<InteractiveContent></InteractiveContent>, document.getElementById('interactive-content'));
+            InteractiveContent.contentComponent = ReactDOM.render(<InteractiveContent></InteractiveContent>, document.getElementById('interactive-content')) as InteractiveContent;
         }
     }
 
     export interface InteractiveContentState {
-
+        maximized: boolean;
     }
 }
