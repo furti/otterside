5e33573a4a81e0755d66242988140641a8358e8e
furti <daniel.furtlehner@gmx.net>
2016-02-05 19:50:41 +0100

    The textarea is scrolled into view after lines are added

--- src/web/scripts/console/ConsoleView.tsx
+++ src/web/scripts/console/ConsoleView.tsx
@@ -1,65 +1,68 @@
 namespace otterside.console {
-  /**
-   * The visual representation of the console.
-   */
-  export class ConsoleView extends React.Component<ConsoleViewProps, ConsoleViewState> {
-      private textarea: ResizeableTextarea;
+    /**
+     * The visual representation of the console.
+     */
+    export class ConsoleView extends React.Component<ConsoleViewProps, ConsoleViewState> {
 
-      constructor() {
-          super();
+        private textarea: ResizeableTextarea;
 
-          this.state = {
-              lines: []
-          };
-      }
+        constructor() {
+            super();
 
-      addLine(line: string): void {
-          this.state.lines.push(line);
+            this.state = {
+                lines: []
+            };
+        }
 
-          this.setState({
-              lines: this.state.lines
-          });
-      }
+        addLine(line: string): void {
+            this.state.lines.push(line);
 
-      /**
-       * Set The focus to the command input
-       */
-      public focusInput(): void {
-          this.textarea.focus();
-      }
+            this.setState({
+                lines: this.state.lines
+            });
 
-      private handleUp(e: React.KeyboardEvent): void {
-          var textarea = e.target as HTMLTextAreaElement;
+            this.textarea.scrollIntoView();
+        }
 
-          if (e.keyCode === 13) {
-              //Enter pressed --> execute the command
-              this.props.onExecute(textarea.value);
-              textarea.value = '';
-          }
-      }
+        /**
+         * Set The focus to the command input
+         */
+        public focusInput(): void {
+            this.textarea.focus();
+        }
 
-      private handleDown(e: React.KeyboardEvent): void {
-          //Prevent the default action on enter or tab. We don't want a newline in the textarea nor we want the focus to be lost.
-          if (e.keyCode === 13 || e.keyCode === 9) {
-              e.preventDefault();
-          }
-      }
+        private handleUp(e: React.KeyboardEvent): void {
+            var textarea = e.target as HTMLTextAreaElement;
 
-      render() {
-          return <div className="console" onClick={(e) => this.focusInput() }>
-              <div className="console-lines">
-                  {
-                      this.state.lines.map((line, index) => {
-                          return <MarkdownParagraph key={"line-" + index} markdownContent={line} className="console-line"></MarkdownParagraph>
-                      })
-                  }
-              </div>
-              <div className="console-input">
-                  <span className="prompt">$</span>
-                  <ResizeableTextarea onKeyUp={(event) => this.handleUp(event) } onKeyDown={(event) => this.handleDown(event) } ref={(textarea) => this.textarea = textarea}>
-                  </ResizeableTextarea>
-              </div>
-          </div>
-      }
-  }
+            if (e.keyCode === 13) {
+                //Enter pressed --> execute the command
+                this.props.onExecute(textarea.value);
+                textarea.value = '';
+            }
+        }
+
+        private handleDown(e: React.KeyboardEvent): void {
+            //Prevent the default action on enter or tab. We don't want a newline in the textarea nor we want the focus to be lost.
+            if (e.keyCode === 13 || e.keyCode === 9) {
+                e.preventDefault();
+            }
+        }
+
+        render() {
+            return <div className="console" onClick={(e) => this.focusInput() }>
+                <div className="console-lines">
+                    {
+                        this.state.lines.map((line, index) => {
+                            return <MarkdownParagraph key={"line-" + index} markdownContent={line} className="console-line"></MarkdownParagraph>
+                        })
+                    }
+                </div>
+                <div className="console-input">
+                    <span className="prompt">$</span>
+                    <ResizeableTextarea onKeyUp={(event) => this.handleUp(event) } onKeyDown={(event) => this.handleDown(event) } ref={(textarea) => this.textarea = textarea}>
+                    </ResizeableTextarea>
+                </div>
+            </div>
+        }
+    }
 }
