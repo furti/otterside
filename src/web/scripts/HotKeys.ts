d970e7d2855f8f994a488e2c21a4230b23c86f25
furti <daniel.furtlehner@gmx.net>
2016-02-08 20:11:47 +0100

    The Main menu is displayed when esc is pressed

--- src/web/scripts/HotKeys.ts
+++ src/web/scripts/HotKeys.ts
@@ -3,12 +3,42 @@ namespace otterside {
     export const enum Key {
         ESC = 27,
         ENTER = 13,
-        TAB = 9
+        TAB = 9,
+        UP = 38,
+        DOWN = 40,
+        LEFT = 37,
+        RIGHT = 39
     }
+
+    export declare type HotkeyHandler = (event: KeyboardEvent) => boolean;
+
+    var registeredHandlers: { [key: number]: HotkeyHandler[] } = {};
 
     export class HotKeys {
-        public static registerHotkey(key: Key): void {
+        /**
+         * Register a handler for a key. When the key is pressed the handler will be called.
+         * @param {Key} key The key to listen for
+         * @param {KeyboardEvent} handler Function that is called when the key is pressed.
+         * @return {boolean} return false if no further handlers should be called.
+         */
+        public static registerHotkey(key: Key, handler: HotkeyHandler): void {
+            if (!registeredHandlers[key]) {
+                registeredHandlers[key] = [];
+            }
 
+            registeredHandlers[key].push(handler);
         }
     }
+
+    document.addEventListener('keypress', function(event: KeyboardEvent) {
+        if (!registeredHandlers[event.keyCode]) {
+            return;
+        }
+
+        for (let handler of registeredHandlers[event.keyCode]) {
+            if (!handler(event)) {
+                return;
+            }
+        }
+    });
 }
--- src/web/scripts/MainMenuState.ts
+++ src/web/scripts/MainMenuState.ts
@@ -1,14 +1,29 @@
 namespace otterside {
+
     export class MainMenuState extends Phaser.State {
         public static stateName = 'MainMenu';
 
-        private console: Console;
-
         public preload() {
-            this.console = new Console('mainmenu');
+            MainMenuConsole.createConsole();
         }
 
         public create() {
+            MainMenuConsole.start();
+        }
+
+        public shutdown() {
+            MainMenuConsole.clear();
+        }
+    }
+
+    class MainMenuConsole {
+        private static console: Console;
+
+        public static createConsole(): void {
+            this.console = new Console('mainmenu');
+        }
+
+        public static start(): void {
             this.console.start().then((console: Console) => {
                 Loading.hide();
                 this.console.maximize();
@@ -17,5 +32,18 @@ namespace otterside {
                 this.console.maximize();
             });
         }
+
+        public static clear(): void {
+            this.console = undefined;
+        }
     }
+
+    HotKeys.registerHotkey(Key.ESC, (event) => {
+        if (!InteractiveContent.contentComponent.isComponentActive()) {
+            MainMenuConsole.createConsole();
+            MainMenuConsole.start();
+        }
+
+        return true;
+    });
 }
